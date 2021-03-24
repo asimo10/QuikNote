dfe38b14dd9fbf17b10c0a980c82d698f23c4361
portaltree <66620496+asimo10@users.noreply.github.com>
2021-03-24 15:42:08 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
-document.getElementById("title").innerHTML = localStorage["title"];
-document.getElementById("content").innerHTML = localStorage["content"];
+var title;
+var content;
+var text;
 
 setInterval(function() {
-  localStorage["title"] = document.getElementById("title").innerHTML;
-  localStorage["content"] = document.getElementById("content").innerHTML;
-}, 1000);
+    title = document.getElementById("title").innerHTML;
+    content = document.getElementById("content").innerHTML;
+}, 10);
+
+function download(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
+
+function save() {
+    var text = '<!DOCTYPE html><html> <head> <title>FlashNote | About</title>  </head> <style>body{color:#333;font-weight:100;max-width:50em;margin:0 auto}hr{display:block;height:1px;border:0;border-top:1px solid #b7b7b7;margin:1em 0;padding:0}a{text-decoration:none}#title{font-family:"Montserrat";src:url(Montserrat-Medium.woff2) format(woff2),url(Montserrat-Medium.woff) format(woff);font-weight:600;font-size:48px;padding-top:30px}#content{font-family:"Montserrat";src:url(Montserrat-Medium.woff2) format(woff2),url(Montserrat-Medium.woff) format(woff);font-size:25px;font-weight:500;padding-top:10px}hr[space]{border-top:0 solid #fff}</style> <body> <hr space> <div id="title" placeholder="Title">' + title + '</div> <div id="content" placeholder="content">' + content + '</div> </body></html>';
+    download("QuikNote.html", text);
+}
+
+document.onkeyup = function(){
+    if (content == "") {
+        document.getElementById("content").className = "tab";
+    }
+    else {
+        document.getElementById("content").className = "tabfocused";
+    }
+}
